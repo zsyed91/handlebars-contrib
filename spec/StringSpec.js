e55933ad0cba40d7409de3d325d9e4b273f0a3ed
Zshawn Syed <zsyed91@gmail.com>
2015-05-08 22:58:36 +0100

    Capitalize and capitalize_all methods with tests

--- spec/StringSpec.js
+++ spec/StringSpec.js
@@ -3,6 +3,37 @@ function(Strings){
  
     describe("String helpers", function(){
 
+        describe("#capitalize", function(){
+
+        
+            it("should capitalize the first letter", function(){
+                expect(Strings.capitalize("word")).toEqual("Word");
+            });
+
+            it ("should capitalize only the first letter", function(){
+                expect(Strings.capitalize("wORD")).toEqual("WORD");
+            });
+
+            it ("should normalize the value in strict mode", function(){
+                expect(Strings.capitalize("wORD", true)).toEqual("Word");
+                expect(Strings.capitalize("MULtiple Words", true)).toEqual("Multiple words");
+                 
+            });
+
+        });
+
+
+        describe("#capitalize_all", function(){
+
+            it("should capitalize each word in the string", function(){
+                expect(Strings.capitalize_all("multiple   words 123")).toEqual("Multiple Words 123");
+                expect(Strings.capitalize_all("1 2 3   456")).toEqual("1 2 3 456");
+                 
+            });
+
+        });
+
+
 
     });
 
--- src/handlebars.strings.js
+++ src/handlebars.strings.js
@@ -3,7 +3,24 @@ define(["handlebars"],
 function(Handlebars){
 
     var strings = {
+        capitalize: function(value, strictMode){
+            strictMode = strictMode || false;
 
+            if (strictMode) {
+                return value.charAt(0).toUpperCase() + value.slice(1).toLowerCase();    
+            }
+            return value.charAt(0).toUpperCase() + value.slice(1);
+        },
+
+        capitalize_all: function(value) {
+            var words = value.split(/\s+/);
+
+            var done = [];
+            for (var i = 0; i<words.length; i++) {
+                done.push(this.capitalize(words[i]));
+            }
+            return done.join(' ');
+        }
     };
 
 
