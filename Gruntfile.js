5833abb787a606637d09218f0f6c16176c1a845c
Zshawn Syed <zsyed91@gmail.com>
2015-05-09 01:08:35 +0100

    Working cli jasmine tests

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,33 +1,35 @@
-/* jshint node: true */
+module.exports = function(grunt) {
+    'use strict';
 
-module.exports = function (grunt) {
-  "use strict";
-
-  grunt.initConfig({
-      pkg: grunt.file.readJSON('package.json')
-    , jshint: {
-        all: [
-            "Gruntfile.js"
-          , "spec/**/*.js"
-          , "src/**/*.js"
-        ]
-      , options: {
-          jshintrc: '.jshintrc'
+    // Project configuration.
+    grunt.initConfig({
+        jasmine : {
+            src : 'src/**/*.js',
+            options : {
+                specs : 'spec/**/*.js',
+                template: require('grunt-template-jasmine-requirejs'),
+                templateOptions: {
+                    requireConfigFile: 'lib/main.js'
+                }
+            }
         },
-      }
-    , jasmine: {
-        src: "lib/jasmine-2.3.2/**/*.js"
-      , options: {
-          specs: "spec/**/*.js"
-        , vendor: "lib/**/*.js"
-        , version: '2.3.2'
-      }
-    }
-  })
+        jshint: {
+            all: [
+                'Gruntfile.js',
+                'src/**/*.js',
+                'spec/**/*.js'
+            ],
+            options: {
+                jshintrc: '.jshintrc'
+            }
+        }
+    });
 
-  grunt.loadNpmTasks('grunt-contrib-jshint')
-  grunt.loadNpmTasks('grunt-contrib-jasmine')
+    grunt.loadNpmTasks('grunt-contrib-jasmine');
+    grunt.loadNpmTasks('grunt-contrib-jshint');
 
-  grunt.registerTask('test', ['jshint', 'jasmine'])
-  grunt.registerTask('default', ['test'])
+    grunt.registerTask('test', ['jshint', 'jasmine']);
+
+    grunt.registerTask('default', ['test']);
+
 };
